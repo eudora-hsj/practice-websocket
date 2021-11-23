534266329422d6a76acebba289fdb0832d4727e4
eudora-hsj <eudora.hsj@gmail.com>
2021-11-23 17:38:03 +0800

    websocket node.js server & client

---
+++ readme.md
@@ -0,0 +1,3 @@
+websocket 練習
+
+Ref: [使用node-js安裝websocket做出多人聊天室/](https://fgchen.com/wpedu/courses/%E4%BD%BF%E7%94%A8node-js%E5%AE%89%E8%A3%9Dwebsocket%E5%81%9A%E5%87%BA%E5%A4%9A%E4%BA%BA%E8%81%8A%E5%A4%A9%E5%AE%A4/)
---
+++ server.js
@@ -0,0 +1,24 @@
+//import express 和 ws 套件
+const express = require('express')
+const SocketServer = require('ws').Server
+
+const PORT = 3000 //指定 port
+
+//創建 express 物件，綁定監聽  port , 設定開啟後在 console 中提示
+const server = express()
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+//將 express 交給 SocketServer 開啟 WebSocket 的服務
+const wss = new SocketServer({ server })
+//當 WebSocket 從外部連結時執行
+wss.on('connection', ws => {
+    console.log('Client connected')
+    ws.on('message', data => {
+        // console.log(data)
+        data = data.toString()  // 收回來是 Buffer 格式、需轉成字串
+        // console.log(data)
+        ws.send(data)
+    })
+    ws.on('close', () => {
+        console.log('Close connected')
+    })
+})
